ac8f446569e1c9686a810601f9c75c4bcf99f9ff
Schaeff <schaeffer.thibaut@gmail.com>
2017-01-28 20:55:17 +0100

    turn into require'able module

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-
 var bebop = require("node-bebop"),
     JSFtp = require('jsftp');
 
-var drone = bebop.createClient(),
-	ipfs_picture_name;
+var drone = bebop.createClient()
 
 var ftp = new JSFtp({
   host: "192.168.42.1"
@@ -11,25 +9,27 @@ var ftp = new JSFtp({
 
 drone.connect(function() {
 	console.log('Connected');
-
-	setTimeout(function() {
-  		p = drone.takePicture();
-	}, 500);
-
-	setTimeout(function() {
-		console.log(ipfs_picture_name)
-	}, 2000)
 });
 
-drone.on('PictureEventChanged', function(e) {
-	if (e.error != 'ok')
+drone.startMission = function() {
+	console.log("startMission!")
+	//TODO INSTRUCTIONS
+	// setTimeout(function() {
+ //  		p = drone.takePicture();
+	// }, 500);
+}
+
+drone.on('PictureEventChanged', handlePictureEventChanged)
+
+function handlePictureEventChanged(e) {
+	if (e.error !== 'ok')
 		return;
 
 	ftp.ls("internal_000/Bebop_2/media", function(err, res) {
 		filename = get_last_file(res).name;
 
 		ftp.get('internal_000/Bebop_2/media/' + filename, 
-			    'images/' + filename, 
+			    '../images/' + filename, 
 			    function(hadErr) {
 		    if (hadErr)
 		      	console.error('There was an error retrieving the file.');
@@ -37,11 +37,12 @@ drone.on('PictureEventChanged', function(e) {
 		      	console.log('File copied successfully!');
 	  	});
 
-	  	ipfs_picture_name = filename;
-	    console.log(ipfs_picture_name)
+	  	drone.imageFileName = filename;
 	});
-});
+}
 
-get_last_file = function(file_array) {
+function get_last_file(file_array) {
 	return file_array.sort((f1, f2) => f1.name.localeCompare(f2.name)).slice(-1)[0];
 }
+
+module.exports = drone;
